1ab8bfcfc21c6afbb765de214e9175d748e00996
hanhuiGithub2018 <39618388+hanhuiGithub2018@users.noreply.github.com>
2019-07-10 16:04:04 +0800

    Update webpack.config.prod.js

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,19 +1,18 @@
+/**
+ *
+ *  webpack 生产环境配置
+ *
+ * */
 
-const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const webpack = require('webpack'); //访问内置的插件
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');//个人觉得这个压缩插件没得啥子用
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');//
 
 module.exports = merge(baseConfig,{
     mode: 'production',
-    entry: path.join(__dirname,'../src/main.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname,'../dist')
-    },
-    devtool: "source-map",
+    devtool: "cheap-module-source-map",
     module: {
         rules: [
             {
@@ -32,11 +31,8 @@ module.exports = merge(baseConfig,{
     plugins: [
         // new webpack.optimize.UglifyJsPlugin(),
         new MiniCssExtractPlugin({
-            filename: '[name].[hash].css',
-            chunkFilename: '[id].[hash].css'
+            filename: 'style.css',
         }),
-        new UglifyJsPlugin({
-            sourceMap: true
-        })
+        new UglifyJsPlugin()
     ]
 });
