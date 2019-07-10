23579464f7055099deb4a79e7f5c174d327cae27
hanhuiGithub2018 <39618388+hanhuiGithub2018@users.noreply.github.com>
2019-07-10 16:03:33 +0800

    Update webpack.config.dev.js

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,15 +1,16 @@
+/**
+ *
+ *  webpack 开发环境配置
+ *
+ * */
 
-const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 
 module.exports = merge(baseConfig,{
     mode: 'development',
-    entry: path.join(__dirname,'../src/main.js'),
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname,'../dist')
-    },
+    devtool: "source-map",
     module: {
         rules: [
             {
@@ -24,17 +25,19 @@ module.exports = merge(baseConfig,{
         // },
         proxy: {
             "/api": {
-                target: "http://localhost:8000",
+                target: "http://localhost:8080",
                 pathRewrite: {"^/api" : "/static/mock"}
             }
         }, //代理服务器，一般使用mock联调  与 vue-cli2.x的 proxyTable 一样的功能
         // open: true,
         host: 'localhost',
-        port: '8000',
+        port: '8080',
         hot: true, //开启热更新
         // contentBase: path.join(__dirname, 'public'), // boolean | string | array, static file location
-        // compress: true, //使用gzip压缩
+        compress: true, //使用gzip压缩
         // noInfo: true, // 开启热重载发生错误警告
     },
-    plugins: [ ]
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 });
